8e2663adfa6d204076bd740b52bf58be13762e90
Nick Adams <nick.adams.software@gmail.com>
2024-12-13 14:56:44 +0000

    Remove commented out vuetify code

--- apps/atr-one/vite.config.ts
+++ apps/atr-one/vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
-
-// See https://github.com/vitejs/vite-plugin-vue/tree/main/packages/plugin-vue
 import vue from '@vitejs/plugin-vue'
-
-// import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
     vue()
-    // vuetify({ autoImport: true })
   ],
   server: {
     hmr: {
